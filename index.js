b75ff675dba569d2757d938e2799816338b28f88
samaviaansari <samaviaansari>
2024-04-19 13:03:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//Declare a constant `answers` and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
